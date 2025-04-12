0f37ff40937aaae706927c86d755b869ed2b2292
Professional <babangida.bob@mail.ru>
2025-04-12 15:14:16 +0300

    pdated database connection settings

--- config/server.ts
+++ config/server.ts
@@ -2,6 +2,7 @@ export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    // Убедитесь, что переменная окружения APP_KEYS настроена правильно
+    keys: env.array('APP_KEYS', ['key1', 'key2']),
   },
 });

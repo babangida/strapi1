e7af69de7676093dc89478b0c3e23282c6005bc0
Professional <babangida.bob@mail.ru>
2025-04-12 15:51:28 +0300

    pdated database connection setti13131ngs

--- config/admin.ts
+++ config/admin.ts
@@ -1,17 +1,17 @@
 export default ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET'),  // Используем переменную окружения для JWT секрета
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT'),  // Используем переменную окружения для соли API токенов
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT'),  // Используем переменную окружения для соли токенов передачи данных
     },
   },
   flags: {
-    nps: env.bool('FLAG_NPS', true),
-    promoteEE: env.bool('FLAG_PROMOTE_EE', true),
+    nps: env.bool('FLAG_NPS', true),  // Флаг для NPS
+    promoteEE: env.bool('FLAG_PROMOTE_EE', true),  // Флаг для продвижения EE версии
   },
 });
--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,10 @@
 export default () => ({});
+module.exports = {
+    'users-permissions': {
+      config: {
+        apiToken: {
+          salt: process.env.API_TOKEN_SALT,  // Переменная окружения для соли токенов
+        },
+      },
+    },
+  };
